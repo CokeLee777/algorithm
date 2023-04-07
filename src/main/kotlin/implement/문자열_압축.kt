c9690e18fccc7ddd6aef6ead140b23d2fb4a9ba7
CokeLee777 <akak640@kyonggi.ac.kr>
2023-04-08 00:21:06 +0900

    fix: 소스코드 구조 수정

--- src/main/kotlin/implement/문자열_압축.kt
+++ src/main/kotlin/implement/문자열_압축.kt
@@ -3,36 +3,34 @@ package implement
 import java.util.*
 import kotlin.math.*
 
-class Solution {
-    fun solution(s: String): Int {
-        var answer = s.length
+fun solution(s: String): Int {
+    var answer = s.length
 
-        //압축 범위를 늘려가며 확인
-        for(unitOfCompress in 1..s.length / 2){
-            var compressedStr = ""              //압축된 문자열
-            var prevStr = s.substring(0, unitOfCompress)  //이전 문자열
-            var continuousCount = 1             //연속된 문자열의 수
-            //단위 크기만큼 증가시키면서 이전 문자열과 비교한다
-            for(i in unitOfCompress until s.length step unitOfCompress){
-                val subStr = if(i + unitOfCompress <= s.length) s.substring(i, i+unitOfCompress) else s.substring(i)
-                when(subStr){
-                    //이전 문자열과 동일하다면 압축 횟수 증가
-                    prevStr -> continuousCount++
-                    //아니라면
-                    else -> {
-                        compressedStr += if(continuousCount > 1) continuousCount.toString() + prevStr else prevStr
-                        //초기화
-                        prevStr = subStr
-                        continuousCount = 1
-                    }
+    //압축 범위를 늘려가며 확인
+    for(unitOfCompress in 1..s.length / 2){
+        var compressedStr = ""              //압축된 문자열
+        var prevStr = s.substring(0, unitOfCompress)  //이전 문자열
+        var continuousCount = 1             //연속된 문자열의 수
+        //단위 크기만큼 증가시키면서 이전 문자열과 비교한다
+        for(i in unitOfCompress until s.length step unitOfCompress){
+            val subStr = if(i + unitOfCompress <= s.length) s.substring(i, i+unitOfCompress) else s.substring(i)
+            when(subStr){
+                //이전 문자열과 동일하다면 압축 횟수 증가
+                prevStr -> continuousCount++
+                //아니라면
+                else -> {
+                    compressedStr += if(continuousCount > 1) continuousCount.toString() + prevStr else prevStr
+                    //초기화
+                    prevStr = subStr
+                    continuousCount = 1
                 }
             }
-            //나머지 문자열 처리
-            compressedStr += if(continuousCount > 1) continuousCount.toString() + prevStr else prevStr
-            //길이 비교
-            answer = min(answer, compressedStr.length)
         }
-
-        return answer
+        //나머지 문자열 처리
+        compressedStr += if(continuousCount > 1) continuousCount.toString() + prevStr else prevStr
+        //길이 비교
+        answer = min(answer, compressedStr.length)
     }
+
+    return answer
 }
